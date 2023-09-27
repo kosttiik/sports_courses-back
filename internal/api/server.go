e91463e1edec05beb7fc5bec7d4fdec19bb2b8d7
kosttiik <krollex2012@gmail.com>
2023-09-27 14:48:59 +0300

    working homepage

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,33 +1,85 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
+
+type Card struct {
+	Title      string
+	Text       string
+	Image      string
+	Details    string
+	Capacity   int
+	Registered int
+}
+
+var cards = []Card{
+	{"Футбол", "Стадион", "image/image.jpg", "Самый популярный вид спорта", 32, 15},
+	{"Баскетбол", "Спорткомплекс", "image/image.jpg", "Самый популярный вид спорта", 32, 19},
+	{"Теннис", "Манеж", "image/image.jpg", "Самый популярный вид спорта", 32, 12},
+	{"Спортивное ориентирование", "Измайлово", "image/image.jpg", "Самый популярный вид спорта", 32, 17},
+	{"Плавание", "Бассейн", "image/image.jpg", "Самый популярный вид спорта", 32, 14},
+}
 
 func StartServer() {
 	log.Println("Server is starting up...")
 
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/image", "resources")
+	r.Static("/css", "templates/css")
+	r.Static("/font", "resources/font")
 
-	r.LoadHTMLGlob("templates/*")
-
-	r.GET("/home", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main Website",
-		})
-	})
-
-	r.Static("/image", "./resources")
+	r.GET("/", loadHome)
+	r.GET("/:title", loadPage)
 
 	r.Run()
 
 	log.Println("Server shutdown.")
+}
+
+func loadHome(c *gin.Context) {
+	card_title := c.Query("card_title")
+
+	if card_title == "" {
+		c.HTML(http.StatusOK, "courses.html", gin.H{
+			"cards": cards,
+		})
+		return
+	}
+
+	foundCards := []Card{}
+	lowerCardTitle := strings.ToLower(card_title)
+	for i := range cards {
+		if strings.Contains(strings.ToLower(cards[i].Title), lowerCardTitle) {
+			foundCards = append(foundCards, cards[i])
+		}
+	}
+
+	c.HTML(http.StatusOK, "courses.html", gin.H{
+		"cards": foundCards,
+	})
+}
+
+func loadPage(c *gin.Context) {
+	title := c.Param("title")
+
+	for i := range cards {
+		if cards[i].Title == title {
+			c.HTML(http.StatusOK, "region.html", gin.H{
+				"Title":      cards[i].Title,
+				"Text":       cards[i].Text,
+				"Image":      "../" + cards[i].Image,
+				"Details":    cards[i].Details,
+				"Capacity":   cards[i].Capacity,
+				"Registered": cards[i].Registered,
+			})
+			return
+		}
+	}
 }
