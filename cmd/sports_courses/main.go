e6c767a2976ee1fe19d0bc7edff0eb692437df23
Konstantin <krollex2012@gmail.com>
2023-12-24 18:53:33 +0300

    add swagger documentation

--- cmd/sports_courses/main.go
+++ cmd/sports_courses/main.go
@@ -6,6 +6,13 @@ import (
 	"sports_courses/internal/pkg/app"
 )
 
+// @title Запись на спортивные курсы МГТУ им. Н. Э. Баумана
+// @version 0.0-0
+
+// @host 127.0.0.1:8080
+// @schemes http
+// @BasePath /
+
 func main() {
 	log.Println("Application started!")
 
--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 	"strconv"
 
+	"sports_courses/docs"
 	"sports_courses/internal/app/ds"
 	"sports_courses/internal/app/dsn"
 	"sports_courses/internal/app/repository"
 
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
 	"github.com/gin-gonic/gin"
 )
+
+// @BasePath /
 
 type Application struct {
 	repo repository.Repository
@@ -35,6 +41,10 @@ func (a *Application) StartServer() {
 	a.r.GET("courses", a.get_courses)
 	a.r.GET("course/:course", a.get_course)
 
+	// swagger
+	docs.SwaggerInfo.BasePath = "/"
+	a.r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
 	a.r.GET("enrollments", a.get_enrollments)
 	a.r.GET("enrollment", a.get_enrollment)
 
@@ -49,14 +59,24 @@ func (a *Application) StartServer() {
 	a.r.PUT("course/delete/:course_title", a.delete_course)
 	a.r.PUT("course/delete_restore/:course_title", a.delete_restore_course)
 	a.r.PUT("enrollment/delete/:enrollment_id", a.delete_enrollment)
+	a.r.PUT("enrollment_to_course/delete", a.delete_enrollment_to_course)
 
-	a.r.DELETE("enrollment_to_course/delete", a.delete_enrollment_to_course)
+	docs.SwaggerInfo.BasePath = "/"
+	a.r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	a.r.Run()
 
 	log.Println("Server shutdown.")
 }
 
+// @Summary Get all existing courses
+// @Schemes
+// @Description Returns all existing courses
+// @Tags courses
+// @Accept json
+// @Produce json
+// @Success 200 {} string
+// @Router /courses [get]
 func (a *Application) get_courses(c *gin.Context) {
 	var title_pattern = c.Query("title_pattern")
 	var location = c.Query("location")
@@ -71,6 +91,13 @@ func (a *Application) get_courses(c *gin.Context) {
 	c.JSON(http.StatusOK, courses)
 }
 
+// @Summary      Adds courses to database
+// @Description  Creates a new course with parameters, specified in json
+// @Tags courses
+// @Accept json
+// @Produce      json
+// @Success      302  {object}  string
+// @Router       /course/add [put]
 func (a *Application) add_course(c *gin.Context) {
 	var course ds.Course
 
@@ -90,6 +117,12 @@ func (a *Application) add_course(c *gin.Context) {
 
 }
 
+// @Summary      Get course
+// @Description  Returns course with given name
+// @Tags         courses
+// @Produce      json
+// @Success      200  {object}  string
+// @Router       /course/:course [get]
 func (a *Application) get_course(c *gin.Context) {
 	var course = ds.Course{}
 
@@ -106,6 +139,13 @@ func (a *Application) get_course(c *gin.Context) {
 
 }
 
+// @Summary      Edits course
+// @Description  Finds course by name and updates its fields
+// @Tags         courses
+// @Accept json
+// @Produce      json
+// @Success      302  {object}  string
+// @Router       /course/edit [put]
 func (a *Application) edit_course(c *gin.Context) {
 	var course ds.Course
 
@@ -125,6 +165,13 @@ func (a *Application) edit_course(c *gin.Context) {
 
 }
 
+// @Summary      Deletes course
+// @Description  Finds course by name and changes its status to "Недоступен"
+// @Tags         courses
+// @Accept json
+// @Produce      json
+// @Success      302  {object}  string
+// @Router       /course/delete/:course_title [put]
 func (a *Application) delete_course(c *gin.Context) {
 	course_title := c.Param("course_title")
 
@@ -140,6 +187,12 @@ func (a *Application) delete_course(c *gin.Context) {
 	c.String(http.StatusFound, "Course was successfully deleted")
 }
 
+// @Summary      Deletes or restores course
+// @Description  Switches course status from "Действует" to "Недоступен" and back
+// @Tags         courses
+// @Produce      json
+// @Success      200  {object}  string
+// @Router       /course/delete_restore/:course_title [get]
 func (a *Application) delete_restore_course(c *gin.Context) {
 	course_title := c.Param("course_title")
 
@@ -153,6 +206,13 @@ func (a *Application) delete_restore_course(c *gin.Context) {
 	c.String(http.StatusFound, "Course status was successfully switched")
 }
 
+// @Summary      Enroll course
+// @Description  Creates a new enrollment and adds current course in it
+// @Tags general
+// @Accept json
+// @Produce      json
+// @Success      302  {object}  string
+// @Router       /enroll [put]
 func (a *Application) enroll_course(c *gin.Context) {
 	var request_body ds.EnrollCourseRequestBody
 
@@ -174,6 +234,12 @@ func (a *Application) enroll_course(c *gin.Context) {
 
 }
 
+// @Summary      Get enrollments
+// @Description  Returns list of all available enrollments
+// @Tags         enrollments
+// @Produce      json
+// @Success      302  {object}  string
+// @Router       /enrollments [get]
 func (a *Application) get_enrollments(c *gin.Context) {
 	var requestBody ds.GetEnrollmentsRequestBody
 
@@ -188,6 +254,14 @@ func (a *Application) get_enrollments(c *gin.Context) {
 	c.JSON(http.StatusFound, enrollments)
 }
 
+// a.r.GET("enrollment", a.get_enrollment)
+// @Summary      Get enrollment
+// @Description  Returns enrollment with given parameters
+// @Tags         enrollments
+// @Accept		 json
+// @Produce      json
+// @Success      302  {object}  string
+// @Router       /enrollment [get]
 func (a *Application) get_enrollment(c *gin.Context) {
 	var enrollment ds.Enrollment
 
@@ -206,6 +280,13 @@ func (a *Application) get_enrollment(c *gin.Context) {
 	c.JSON(http.StatusFound, found_enrollment)
 }
 
+// @Summary      Edits enrollment
+// @Description  Finds enrollment and updates it fields
+// @Tags         enrollments
+// @Accept json
+// @Produce      json
+// @Success      201  {object}  string
+// @Router       /enrollment/edit [put]
 func (a *Application) edit_enrollment(c *gin.Context) {
 	var enrollment ds.Enrollment
 
@@ -224,6 +305,13 @@ func (a *Application) edit_enrollment(c *gin.Context) {
 	c.String(http.StatusCreated, "Enrollment was successfuly edited")
 }
 
+// @Summary      Changes enrollment status as moderator
+// @Description  Changes enrollment status to any available status
+// @Tags         enrollments
+// @Accept json
+// @Produce      json
+// @Success      201  {object}  string
+// @Router       /enrollment/status_change/moderator [put]
 func (a *Application) enrollment_mod_status_change(c *gin.Context) {
 	var requestBody ds.ChangeEnrollmentStatusRequestBody
 
@@ -254,6 +342,14 @@ func (a *Application) enrollment_mod_status_change(c *gin.Context) {
 	c.String(http.StatusCreated, "Enrollment status was successfully changed")
 }
 
+// Ping godoc
+// @Summary      Changes enrollments status as user
+// @Description  Changes enrollment status as allowed to user
+// @Tags         enrollments
+// @Accept json
+// @Produce      json
+// @Success      201  {object}  string
+// @Router       /enrollment/status_change/user [put]
 func (a *Application) enrollment_user_status_change(c *gin.Context) {
 	var requestBody ds.ChangeEnrollmentStatusRequestBody
 
@@ -272,6 +368,13 @@ func (a *Application) enrollment_user_status_change(c *gin.Context) {
 	c.String(http.StatusCreated, "Enrollment status was successfully changed")
 }
 
+// @Summary      Deletes enrollment
+// @Description  Changes enrollment status to "Удалён"
+// @Tags         enrollments
+// @Accept json
+// @Produce      json
+// @Success      302  {object}  string
+// @Router       /enrollment/delete/:enrollment_id [put]
 func (a *Application) delete_enrollment(c *gin.Context) {
 	enrollment_id, _ := strconv.Atoi(c.Param("enrollment_id"))
 
@@ -285,6 +388,13 @@ func (a *Application) delete_enrollment(c *gin.Context) {
 	c.String(http.StatusFound, "Enrollment was successfully deleted")
 }
 
+// @Summary      Deletes enrollment_to_course connection
+// @Description  Deletes course from enrollment
+// @Tags         enrollments
+// @Accept json
+// @Produce      json
+// @Success      201  {object}  string
+// @Router       /enrollment_to_course/delete [put]
 func (a *Application) delete_enrollment_to_course(c *gin.Context) {
 	var requestBody ds.DeleteEnrollmentToCourseRequestBody
 
